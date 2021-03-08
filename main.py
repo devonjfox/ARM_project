2c243da29c6cb42c5717f7c72765e3daf77d62e5
Brandon Fox <bfox@foxtrot.email>
2021-03-08 18:30:27 -0500

    Filter data for Census

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas
+import os
 
 
 def load_csv_into_dataframe(location_of_csv):
@@ -41,52 +42,22 @@ def depression_rate(census_dataframe, state_name, year, basedataframe, state_cod
 
 
 if __name__ == '__main__':
-    csv = "MHCLD_PUF_2018.csv"
-    df = load_csv_into_dataframe(csv)
-    csv2 = "Census.csv"
-    df2 = pandas.read_csv(csv2, index_col=0)
+    # Returns the absolute path for the current file location
+    dirname = os.path.dirname(__file__)
 
-    # rate_of_depression = depression_rate(df2, '.Oregon', '2018', df, 41)
-    # print("Rate of Depression: " + str(rate_of_depression))
+    # Load the MHCLD into a dataframe
+    mhcld_puf_2018_csv = os.path.join(dirname, 'data/MHCLD_PUF_2018.csv')
+    mhcld = load_csv_into_dataframe(mhcld_puf_2018_csv)
+    print(mhcld.head())
 
-    list_of_states = {'.Oregon': 41, '.Alabama': 1, '.Arizona': 4, '.California': 6}
+    # Clean MHCLD Dataframe
 
-    for state, state_code in list_of_states.items():
-        rate_of_depression = depression_rate(df2, state, '2018', df, state_code)
-        print("Rate of Depression in " + state + ": " + str(rate_of_depression) + "\n")
+    # Load the Census data into a dataframe
+    census_csv = os.path.join(dirname, 'data/Census.csv')
+    census = pandas.read_csv(census_csv, index_col=0)
+    print(census.head())
 
-#     np_array = df.to_numpy()
-#     #SE_mean = numpy.std(np_array, ddof = 1) / numpy.sqrt(numpy.size(np_array))
-#     #print(SE_mean)
-#     # outpatient = df.loc[df['OPISERVICE'] == 2]
-#     Trauma = df.loc[df['MH1'] == 1]
-#     Depression = df.loc[df['MH1'] == 7]
-#     No_depression = df.loc[df['MH1'] != 7]
-#     Trauma_depression = Trauma.loc[Trauma['MH2'] == 7]
-#     print("Trauma: ", (len(Trauma.index)))
-#     print(Trauma)
-#     print("Depression: ", (len(Depression.index)))
-#     print("No_depression: ", (len(No_depression.index)))
-#     print("Trauma_depression: ", (len(Trauma_depression.index)))
-# # Do we need to see depression/trauma in MH1, MH2 and MH3 to find odds?
-# # Calculate odds of exposure (trauma) and having depression (outcome)
-# # Odds for depression in total population = Those with depression/Those without depression
-#     Odds_depression = (len(Depression.index))/(len(No_depression.index))
-#     print("Odds_depression: ", (Odds_depression))
-# # Odds for depression with trauma = those with depression and trauma/those with trauma
-#     Odds_depression_trauma = (len(Trauma_depression.index))/(len(Trauma.index))
-#     print("Odds_depression_trauma: ", (Odds_depression_trauma))
-# # Odds Ratio for exposure (trauma) = odds of depression with trauma/odds of depression
-#     Odds_ratio_depression_trauma = Odds_depression_trauma/Odds_depression
-#     print("Odds_ratio_depression_trauma: ", (Odds_ratio_depression_trauma))
-# # Eq 1 = Risk of depression with exposure (trauma) = Those with depression/All those with trauma
-#     Risk_depression_trauma = (len(Trauma_depression.index))/(len(Trauma.index))
-#     print("Risk_depression_trauma: ", (Risk_depression_trauma))
-# # Eq 2 = Risk of depression in population = Those with depression/total sample
-#     Risk_depression = (len(Depression.index))/(len(df.index))
-#     print("Risk_depression: ", (Risk_depression))
-# # Relative risk = Eq 1/Eq 2
-#     Relative_risk = Risk_depression_trauma/Risk_depression
-#     print("Relative_risk: ", (Relative_risk))
-#
-# # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Clean Census Dataframe
+    # Filter 2018 Data
+    census_2018 = census.loc[:, '2018']
+    print(census_2018.head())

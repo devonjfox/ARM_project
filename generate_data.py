c537b5e4f3ace2d4fb3855885d58ae4d32603b9d
Brandon Fox <bfox@foxtrot.email>
2021-03-09 18:51:32 -0500

    I am a god among men. Worship me!!!

--- generate_data.py
+++ generate_data.py
@@ -29,8 +29,9 @@ if __name__ == '__main__':
 
     # Create the dataframe from the collection of series
     # This is the numbers of occurrences of each FLG by state code
-    diagnosis_by_state_dataframe = pandas.DataFrame(diagnosis_by_state)
-    print(diagnosis_by_state_dataframe)
+    diagnosis_by_state_dataframe = pandas.DataFrame.from_dict(diagnosis_by_state)
+    diagnosis_by_state_dataframe.drop(inplace=True, index=72)
+    diagnosis_by_state_dataframe.drop(inplace=True, index=99)
 
     # Load the Census data into a dataframe
     census_csv = os.path.join(dirname, 'data/Census.csv')
@@ -41,20 +42,30 @@ if __name__ == '__main__':
     census_2018 = census.loc[:, '2018']
     print(census_2018)
 
-    list_of_states = {'.Alabama': 1, '.Arizona': 4, '.California': 6, '.Colorado': 8, '.Connecticut': 9,
+    list_of_states = {'.Arizona': 4, '.California': 6, '.Colorado': 8, '.Connecticut': 9,
                       '.Delaware': 10, '.District of Columbia': 11, '.Florida': 12, '.Hawaii': 15, '.Idaho': 16,
                       '.Illinois': 17, '.Indiana': 18, '.Iowa': 19, '.Kentucky': 21, '.Louisiana': 22, '.Maryland': 24,
                       '.Massachusetts': 25, '.Michigan': 26, '.Minnesota': 27, '.Mississippi': 28, '.Missouri': 29,
-                      '.Montana': 30, '.Nebraska': 31,
+                      '.Montana': 30, '.Nebraska': 31, '.Washington': 53, '.Utah': 49, '.Vermont': 50, '.Texas': 48,
+                      '.Virginia': 51, '.Arkansas': 5,
                       '.Nevada': 32, '.New Mexico': 35, '.New York': 36, '.North Carolina': 37, '.North Dakota': 38,
                       '.Ohio': 39, '.Oklahoma': 40, '.Oregon': 41, '.Pennsylvania': 42, '.Rhode Island': 44,
-                      '.South Carolina': 45,
-                      '.South Dakota': 46, '.Tennessee': 47, '.Texas': 48, '.Utah': 49, '.Vermont': 50, '.Virginia': 51,
-                      '.Washington': 53, '.West Virginia': 54, '.Wisconsin': 55, '.Wyoming': 56}
+                      '.South Carolina': 45, '.South Dakota': 46, '.Alabama': 1, '.Tennessee': 47, '.West Virginia': 54,
+                      '.Wisconsin': 55, '.Wyoming': 56}
 
-    for name, code in list_of_states.items():
-        print(code)
-        print(name)
-        diagnosis_by_state_dataframe.iloc[code].apply(lambda x, y: x / y, args=(code,))
 
-    print(diagnosis_by_state_dataframe)
+    def calc_rate(smaller, bigger):
+        return (smaller / bigger) * 100
+
+
+    for state_name, state_population in census_2018.iteritems():
+        code = list_of_states.get(state_name)
+        if code is None:
+            continue
+        population = int(state_population.replace(',', ''))
+        diagnosis_by_state_dataframe.loc[code] = diagnosis_by_state_dataframe.loc[code].map(
+            lambda x: calc_rate(x, population), na_action='ignore')
+
+    with pandas.option_context('display.max_rows', None, 'display.max_columns',
+                               None):  # more options can be specified also
+        print(diagnosis_by_state_dataframe)
